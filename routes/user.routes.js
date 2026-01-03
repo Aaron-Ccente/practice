af08be548dc71fee015e3a1e144679fb2910a028
Aaron-Ccente <aronccente@gmail.com>
2026-01-02 20:45:48 -0500

    feat: routas RestFull para usuario

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -10,12 +10,12 @@ router.get("/", UserController.getAllUsers);
 router.get("/:id", UserController.getUserById)
 
 // Ruta para crear un usuario
-// router.post("/", UserController.createUser)
+router.post("/", UserController.createUser)
 
 // Ruta para editar informacion del usuario
-// router.put("/:id", UserController.editUserInfo)
+router.put("/:id", UserController.editUserInfo)
 
 // Ruta para eliminar un usuario
-// router.delete("/:id", UserController.deleteUser)
+router.delete("/:id", UserController.deleteUser)
 
 export default router
